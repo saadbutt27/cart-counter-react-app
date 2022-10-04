f54e4f02298fc1a46c002317361d362661960cf2
Saad Nisar Butt <sb03750@gmail.com>
2022-10-04 14:55:00 +0500

    added navbar.jsx and work more precise

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,18 +2,19 @@ import React, { Component } from "react";
 // import List from "./list/List";
 
 class Counter extends Component {
-  state = {
-    value: this.props.value,
-  };
+  // state = {
+  //   value: this.props.counter.value,
+  // };
 
   //   constructor() {
   //     super();
   //     this.handleIncrement =  this.handleIncrement.bind(this);
   //   }
 
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
+  // handleIncrement = () => {
+  //   this.setState({ value: this.state.value + 1 });
+  // };
+
   //   styles = {
   //     fontSize: 50,
   //     fontWeight: "bold",
@@ -23,12 +24,17 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-        <button className="btn btn-danger btn-sm m-2">Delete</button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
         {/* <List /> */}
       </div>
     );
@@ -36,12 +42,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { value: count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? "Zero" : count;
   }
 }
---
+++ src/components/navbar.jsx
@@ -0,0 +1,24 @@
+// import React, { Component } from "react";
+
+// Stateless Functional Component
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#index.html">
+          Navbar{" "}
+          <span className="badge badge-pill badge-secondary">
+            {totalCounters}
+          </span>
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+// class NavBar extends Component {
+//   render() {
+//   }
+// }
+
+export default NavBar;
